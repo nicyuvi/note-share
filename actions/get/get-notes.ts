7356ed356205a0860df4f73e634aa3311d758c03
Nicolis Yuvienco <nickyuvienco@gmail.com>
2023-12-08 11:02:32 -0800

    update getNotes to only get notes related to current user

--- actions/get/get-notes.ts
+++ actions/get/get-notes.ts
@@ -1,10 +1,18 @@
 'use server'
 import db from '@/lib/db'
+import { auth } from '@clerk/nextjs'
 
 // TODO: get notes related to current user
 export async function getNotes() {
+  const { userId } = auth()
+  if (!userId) return { error: 'Failed to get current user' }
+
   try {
-    const notes = await db.note.findMany()
+    const notes = await db.note.findMany({
+      where: {
+        authorId: userId,
+      },
+    })
     return { success: notes }
   } catch (e) {
     return { error: 'Failed to get notes' }
