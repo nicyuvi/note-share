29a1b92612ddac38be33f04e2e48055c443e45f0
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-01-16 20:00:30 -0800

    remove unused imports

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,7 +1,5 @@
 import MainNav from '@/components/layout/main-nav'
 import Sidebar from '@/components/layout/sidebar'
-import { getProfile } from '@/actions/get/get-profile'
-import { redirect } from 'next/navigation'
 
 export default async function DashboardLayout({
   children,
