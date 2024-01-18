f58ebd607f387d7fe6d1b9d67d8feba1958cbaf5
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-01-17 22:42:50 -0800

    update profile page

--- app/(main)/(routes)/profile/page.tsx
+++ app/(main)/(routes)/profile/page.tsx
@@ -2,6 +2,7 @@ import { getProfile } from '@/actions/get/get-profile'
 import type { Profile } from '@prisma/client'
 import { notFound } from 'next/navigation'
 import { UserButton } from '@clerk/nextjs'
+import { Separator } from '@/components/ui/separator'
 
 const Profile = async () => {
   const response = await getProfile()
@@ -10,9 +11,12 @@ const Profile = async () => {
 
   return (
     <>
-      <p>{profile.name}</p>
+      <div className="mb-6 flex items-center justify-between">
+        <h1 className="text-3xl font-bold text-hub-600">{profile.name}</h1>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+      <Separator className="mb-6 bg-hub-600" />
       <p>{profile.bio}</p>
-      <UserButton afterSignOutUrl="/" />
     </>
   )
 }
