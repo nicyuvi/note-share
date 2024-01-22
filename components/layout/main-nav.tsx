0893f6809914840aed4c678538cfbe43911db182
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-01-21 23:12:07 -0800

    adjust main nav padding

--- components/layout/main-nav.tsx
+++ components/layout/main-nav.tsx
@@ -1,7 +1,5 @@
 const MainNav = () => {
-  return (
-    <div className="container bg-hub-500 text-hub-600 font-bold">NoteHub</div>
-  )
+  return <div className="bg-hub-500 px-2 font-bold text-hub-600">NoteHub</div>
 }
 
 export default MainNav
